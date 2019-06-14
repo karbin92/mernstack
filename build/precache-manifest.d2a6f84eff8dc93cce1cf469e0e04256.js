60c7a2de7411e60d26e58cc1c66b9556229210ca
Karbin92 <fredrik.karlsson1992@live.com>
2019-06-14 12:34:30 +0200

    jaja

---
+++ build/precache-manifest.d2a6f84eff8dc93cce1cf469e0e04256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ab25a8a8968d57e2be2e24ab72d76c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "680efdf5fec7108c960b",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "680efdf5fec7108c960b",
+    "url": "/static/js/main.94770818.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
