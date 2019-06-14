2bd4ed0ef00ab8a8a47e8a3b56444c493cbcbe5b
Karbin92 <fredrik.karlsson1992@live.com>
2019-06-14 12:10:22 +0200

    aha

---
+++ build/precache-manifest.0a185d3b5de63d3642698fcdbec42435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c049edb58e3d70fb734f19ca74b9ebd",
+    "url": "/mernstack/index.html"
+  },
+  {
+    "revision": "0dbd0b610b44a4af3080",
+    "url": "/mernstack/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/mernstack/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "0dbd0b610b44a4af3080",
+    "url": "/mernstack/static/js/main.8bbf7d60.chunk.js"
+  },
+  {
+    "revision": "e6e9e145fef6a2b315d1",
+    "url": "/mernstack/static/js/runtime~main.e04b6ea2.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/mernstack/static/media/logo.ee7cd8ed.svg"
+  }
+]);
