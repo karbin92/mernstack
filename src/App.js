9a8c961b66890c8e0d8bcc3787ff358f840034fd
Karbin92 <fredrik.karlsson1992@live.com>
2019-06-15 22:33:36 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Customers from './Components/Customers/Customers';
 function App() {
   return (
     <div className="App">
-        <p>
-          Change <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://artportalen.se"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
       
+          <Customers/>
     </div>
   );
 }
