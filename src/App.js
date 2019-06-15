832aa131e2a8386644a20ef9d939e7ad5e5e8c17
Karbin92 <fredrik.karlsson1992@live.com>
2019-06-15 22:41:07 +0200

    y

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Customers from './Components/Customers/Customers';
+
 function App() {
   return (
     <div className="App">
@@ -14,7 +14,7 @@ function App() {
           Learn React
         </a>
       
-          <Customers/>
+        
     </div>
   );
 }
